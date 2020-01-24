de4736d10e45a1d2eb6737899cd44b4a91407160
Mitchell <mitchell@wintermute-2.local>
2020-01-24 08:41:20 -0800

    add session_id to reservation table

--- server/models/reservationModel.js
+++ server/models/reservationModel.js
@@ -2,7 +2,11 @@ var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
 var reservationSchema = new Schema({
-	'email' : String
+	'email' : String,
+	'session_id' : {
+	 	type: Schema.Types.ObjectId,
+	 	ref: 'session'
+	}
 });
 
 module.exports = mongoose.model('reservation', reservationSchema);
